4bf428590efa0079e64ce8695324f27a6c86d47b
Linus Schoemaker <linus@clipperhq.com>
2016-02-24 11:25:14 +0100

    Create login view

--- social/urls.py
+++ social/urls.py
@@ -4,4 +4,5 @@ from social import views
 urlpatterns = [
     url(r'$^',     views.index, name='index'),
     url(r'login/', views.login, name='login'),
+    url(r'home/',  views.home, name='home'),
 ]
--- social/views.py
+++ social/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'social/index.html')
 
+
 def login(request):
+   user = authenticate(username=request.POST['username'], password=request.POST['password']) 
+   if user is not None:
+       return HttpResponseRedirect(reverse('social:home'))
+   else:
+       return HttpResponseBadRequest("The combination of username and password does not exist.")
+
+def home(request):
     pass
+
+           
