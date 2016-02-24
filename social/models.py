92974484f5ac91bf8a09afe7064128ec95198f90
Linus Schoemaker <linus@clipperhq.com>
2016-02-24 10:59:14 +0100

    Make first models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    text = models.TextField()
+    poster = models.ForeignKey(User)
+    date_time = models.DateTimeField(auto_now=True)
+    photo = models.ImageField()
+
+class Comment(models.Model):
+    text = models.TextField()
+    poster = models.ForeignKey(User)
+    post = models.ForeignKey(Post)
+    date_time = models.DateTimeField(auto_now=True)
